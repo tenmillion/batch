b22a3ddad45c10f312f5ae96de769acdcbc9cdfc
Yoriko Yamamura <yoriko-y@is.naist.jp>
2013-09-25 09:36:19 +0900

    trying different values. larger aee, aei

--- batch.sh
+++ batch.sh
@@ -1,9 +1,9 @@
 # cat batch.sh
-for pulse in 3.0 #1.0 9.0 #1.7320508075688772 5.196152422706632
+for pulse in 25 50 100 #1.0 9.0 #1.7320508075688772 5.196152422706632
 do
-    for aei in 0.1 0.5 1.0
+    for aei in 5.0 10.0 12.0
     do
-   	for aee in 0.1 0.5 1.0 5.0 10.0
+   	for aee in 5.0 10.0 12.0
        	do
             if ! test -s "*_aee$aeeaei$aeiphi$phipulse$pulse.txt"; then
                     ~/python batch_model.py 1.0 0.1 $aee $aei 0.1 2.0 3.0 $pulse
--- batch1.sh
+++ batch1.sh
@@ -1,13 +1,13 @@
 # cat batch.sh
-for phi in 1.0 #1.0 9.0 #1.7320508075688772 5.196152422706632
+for pulse in 25 50 100 #1.0 9.0 #1.7320508075688772 5.196152422706632
 do
-    for aei in 0.1 0.5 1.0
-    do
-   	for aee in 0.1 0.5 1.0 5.0 10.0
-       	do
-            if ! test -s "*_aee$aeeaei$aeiphi$phipulse$pulse.txt"; then
-                    ~/python batch_model.py 1.0 0.1 $aee $aei 0.1 2.0 $phi 25
-            fi
-        done
-    done
+for aei in 5.0 10.0 12.0
+do
+for aee in 5.0 10.0 12.0
+do
+if ! test -s "*_aee$aeeaei$aeiphi$phipulse$pulse.txt"; then
+~/python batch_model.py 1.0 0.1 $aee $aei 0.1 2.0 1.0 $pulse
+fi
+done
+done
 done
